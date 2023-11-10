d61815ebb36da233f9d4a673ce565f6451f730e1
shakilsquad <freelancershakilsquad@gmail.com>
2023-11-10 23:51:08 +0600

    search option add

--- src/Componant/Donation/Donation.jsx
+++ src/Componant/Donation/Donation.jsx
@@ -1,19 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { MyContext } from '../../Context/MyProvider';
 import ExtraCart from '../ExtraCart/ExtraCart';
 
 const Donation = () => {
     const { addData, setAddData } = useContext(MyContext)
     console.log(addData)
+
+    const [dataLength, setDataLength] = useState(4);
     return (
         <div className='container mx-auto'>
             <div className='grid md:grid-cols-2 gap-5'>
                 {
-                    addData.map(donationAdd => <ExtraCart key={donationAdd.id} donationAdd={donationAdd}></ExtraCart>)
+                    addData.slice(0, dataLength).map(donationAdd => <ExtraCart key={donationAdd.id} donationAdd={donationAdd}></ExtraCart>)
 
                 }
             </div>
-            
+            <div
+            class='ml-[50%] '
+                className={ dataLength === addData.length && 'hidden'}>
+                <button
+                    onClick={() => setDataLength(addData.length)}
+                    className='py-2 px-4 bg-[#F87147] text-lg font-bold text-white rounded-lg'>Show All </button>
+            </div>
         </div>
     );
 };
--- src/Componant/Home/Home.jsx
+++ src/Componant/Home/Home.jsx
@@ -4,6 +4,8 @@ import Cart from '../Cart/Cart';
 
 const Home = () => {
     const [carts, setCart] = useState([]);
+    const [value, searchValue] = useState ('');
+    const [filterData, setFilterData] = useState ([]);
     // const [cartsdata, setCartData] = useState([]);
 
     useEffect(() => {
@@ -11,6 +13,16 @@ const Home = () => {
             .then(res => res.json())
             .then(data => setCart(data));
     }, [])
+
+    useEffect(() => {
+        console.log(filterData);
+      }, [filterData]);
+
+     const handelSearch = () => {
+        let flt = carts.filter(itm => itm.category.toLowerCase().includes(value.toLowerCase()))
+        setFilterData(flt)
+        console.log(filterData);
+    }
     return (
 
         <div >
@@ -20,20 +32,27 @@ const Home = () => {
                 '>
                     <h1 className='text-xl md:text-3xl lg:text-5xl font-medium py-3 md:py-6 lg:py-10'>I Grow By Helping People In Need</h1>
                     <div className="flex items-center justify-center">
-                        <input className='  rounded-l-lg' type="search" name="search" id="" placeholder='Search here....' />
-                        <button className='py-2 px-4 bg-[#FF444A]  rounded-r-lg text-white '>Search</button>
+
+                        <input  onChange={(e) => searchValue(e.target.value)} className='  rounded-l-lg' type="search" name="search" id="" placeholder='Search here....' />
+                        <button onClick={handelSearch}
+                        className='py-2 px-4 bg-[#FF444A]  rounded-r-lg text-white '>Search</button>
                     </div>
                 </div>
             </div>
 
             <dir className="grid md:grid-cols-2 lg:grid-cols-4 gap-5 container mx-auto py-8" >
                 {
+                    value == "" ?   carts.map(cart => <Cart  key={cart.id} cart={cart}></Cart>) :  filterData.map(cart => <Cart  key={cart.id} cart={cart}></Cart>)
+                }
+                {
                     // carts.map(carts => <Cart key={carts.id} cart={carts} ></Cart>)
-                    carts.map(cart => <Cart  key={cart.id} cart={cart}></Cart>)
+
+
                 }
             </dir>
         </div>
     );
 };
+
 
 export default Home;
